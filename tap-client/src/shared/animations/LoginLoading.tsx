4d06feb64892a3eaec8a7b5a6f97f24ae1c0b555
Romerico <mrh.jrd.234@gmail.com>
2024-11-19 12:33:38 -0500

    Set up lottie image animation for login loading screen

---
+++ tap-client/src/shared/animations/LoginLoading.tsx
@@ -0,0 +1,11 @@
+import Lottie from "lottie-react";
+import loginAnimation from "../../assets/auth-assets/login-animation.json";
+
+export default function LoginAnimation() {
+    // TODO: Use the Lottie image to render the login animation
+
+    return (
+        <div>
+        </div>
+    );
+}
