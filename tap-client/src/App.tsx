57ad1ef28525b64ebbae4cfcf482ebdb80a59d86
Romerico <mrh.jrd.234@gmail.com>
2024-11-26 16:55:45 -0500

    Pulled fresh branch and add forms component

--- tap-client/src/App.tsx
+++ tap-client/src/App.tsx
@@ -6,6 +6,7 @@ import FooterComponent from "./components/footer/FooterComponent";
 import LoginComponent from "./components/auth/LoginComponent";
 import RegisterComponent from "./components/auth/RegisterComponent";
 import AuthProvider from "./components/auth/AuthComponent";
+import FormComponent from "./components/form/FormComponent";
 
 export default function App() {
     return (
@@ -16,6 +17,7 @@ export default function App() {
                     <Route path="/home" element={<HomeComponent />} />
                     <Route path="/login" element={<LoginComponent />} />
                     <Route path="/register" element={<RegisterComponent />} />
+                    <Route path="/preferences-form" element={<FormComponent />} />
                     <Route path="*" element={<HomeComponent />} />
                 </Routes>
                 <FooterComponent />
