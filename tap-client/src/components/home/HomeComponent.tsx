88f06c73398d59b0b195f1886bc049cbef21074a
Gray <mitchellbadri@gmail.com>
2024-11-30 18:55:36 -0500

    Request Compliance

--- tap-client/src/components/home/HomeComponent.tsx
+++ tap-client/src/components/home/HomeComponent.tsx
@@ -1,27 +1,51 @@
 import "./HomeComponent.css";
+import studentImg from "../../../src/assets/home-assets/stock-students.jpg"
+import planImg from "../../../src/assets/home-assets/stock-plan.jpg"
+import aiImg from "../../../src/assets/home-assets/stock-ai.jpg"
+import collegeImg from "../../../src/assets/home-assets/stock-TU.jpg"
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomeComponent() {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/about');
+    }
+
     return (
         <div>
-            <div className="Header">Towson Academic Planner</div>
-            <div
-                className="text-2xl"
-                style={{ textAlign: "center", margin: "100px" }}
-            >
-                TAP is made by and for students. Here, find all your enrollment
-                needs and plan for a successful college experience.
+            <div className="flex mt-10 h-screen items-center justify-center">
+                <img src={collegeImg} alt="Lanscape photo of Towson University" className="w-[50%]"/>
+                <div className="p-5 mt-5 w-[30%] m-[1.5%] float-left">
+                    <p className="text-2xl text-center">
+                        The Mission
+                    </p>
+                    <div className="w-full p-8 text-center h-[200px]">
+                        Towson Academic Pathway is a student-made and student-run service. We are dedicated to 
+                        providing exceptional planning services to help you achieve your goals during your
+                        time at Towson University. 
+
+                        <br />
+
+                        <button
+                            onClick={handleClick}
+                            className="px-3 py-1 mt-3 bg-towsonGold text-white font-medium rounded-lg hover:bg-towsonGoldDark transition"
+                        >
+                            Meet the Team
+                        </button>
+                    </div>
+                </div>
             </div>
 
-            <div className="subHeader text-3xl">Features</div>
-            <div className="Row">
-                <div
-                    className="Card"
-                    style={{ width: "30%", float: "left", margin: "1.5%" }}
-                >
-                    <p className="text-xl" style={{ textAlign: "center" }}>
+            <div className="p-5 text-center bg-towsonGold mt-5 text-3xl">Features</div>
+            <div className="flex mt-10 h-screen items-center justify-center">
+                <div className="p-5 mt-5 w-[30%] m-[1.5%] float-left">
+                    <p className="text-2xl text-center">
                         Plan your Degree
                     </p>
-                    <div className="textBox" style={{ height: 200 }}>
+                    <div className="w-full p-8 text-center h-[200px]">
                         Plan your degree from your first day on campus to your
                         gradutation date all in one place. Ensure from the
                         beginning that you cover every credit and prerequisite
@@ -29,14 +53,16 @@ export default function HomeComponent() {
                     </div>
                 </div>
 
-                <div
-                    className="Card"
-                    style={{ width: "30%", float: "left", margin: "1.5%" }}
-                >
-                    <p className="text-xl" style={{ textAlign: "center" }}>
+                <img src={studentImg} alt="Students on steps" className="w-[50%]"/>
+            </div>
+
+            <div className="flex mt-10 h-screen items-center justify-center">
+                <img src={planImg} alt="Person planning" className="w-[50%]"/>
+                <div className="p-5 mt-5 w-[30%] m-[1.5%] float-left">
+                    <p className="text-2xl text-center">
                         Class Catalog
                     </p>
-                    <div className="textBox" style={{ height: 200 }}>
+                    <div className="w-full p-8 text-center h-[200px]">
                         View Towson's course catalog all in one place helps you
                         get a picture of what is available to you. Using our
                         filtering tools, look for a specific class, view a whole
@@ -44,47 +70,41 @@ export default function HomeComponent() {
                         schedule filler.
                     </div>
                 </div>
+            </div>
 
-                <div
-                    className="Card"
-                    style={{ width: "30%", float: "left", margin: "1.5%" }}
-                >
-                    <p className="text-xl" style={{ textAlign: "center" }}>
-                        AI Assitance
+            <div className="flex mt-10 h-screen items-center justify-center">
+                <div className="p-5 mt-5 w-[30%] m-[1.5%] float-left">
+                    <p className="text-2xl text-center">
+                        AI Assistance
                     </p>
-                    <div className="textBox" style={{ height: 200 }}>
+                    <div className="w-full p-8 text-center h-[200px]">
                         Take advantage of the explosion in AI technologies to
                         help you plan your degree. Our application uses AI to
                         organize your classes into the optimal order for you to
                         achieve your academic goals at Towson University.
                     </div>
                 </div>
-            </div>
-            <br />
 
-            <div className="subHeader text-3xl">Meet The Team</div>
-            <div className="Row">
-                <div
-                    className="Card"
-                    style={{ width: "30%", float: "left", margin: "1.5%" }}
-                >
-                    <p className="text-xl" style={{ textAlign: "center" }}>
+                <img src={aiImg} alt="Human hand touching a robot hand" className="w-[50%]"/>
+            </div><br />
+
+            {/* <div className="p-5 text-center bg-towsonGold mt-5 text-3xl">Meet The Team</div>
+            <div className="flex">
+                <div className="p-5 mt-5 w-[30%] m-[1.5%] float-left">
+                    <p className="text-xl text-center">
                         Scheduling Assistant
                     </p>
-                    <div className="textBox" style={{ height: 150 }}>
+                    <div className="w-full p-8 text-center h-[150px]">
                         Hello, I'm Romerico. I'm a student at Towson University
                         and I'm interested in software engineering.
                     </div>
                 </div>
 
-                <div
-                    className="Card"
-                    style={{ width: "30%", float: "left", margin: "1.5%" }}
-                >
-                    <p className="text-xl" style={{ textAlign: "center" }}>
+                <div className="p-5 mt-5 w-[30%] m-[1.5%] float-left">
+                    <p className="text-xl text-center">
                         Timothy DeLloyd
                     </p>
-                    <div className="textBox" style={{ height: 150 }}>
+                    <div className="w-full p-8 text-center h-[150px]">
                         Hello, I'm Tim. I'm a Towson University math major
                         interested in data analytics, operations research,
                         machine learning, artificial intelligence, programming,
@@ -92,19 +112,16 @@ export default function HomeComponent() {
                     </div>
                 </div>
 
-                <div
-                    className="Card"
-                    style={{ width: "30%", float: "left", margin: "1.5%" }}
-                >
-                    <p className="text-xl" style={{ textAlign: "center" }}>
+                <div className="p-5 mt-5 w-[30%] m-[1.5%] float-left">
+                    <p className="text-xl text-center">
                         Mitchell Griff
                     </p>
-                    <div className="textBox" style={{ height: 150 }}>
+                    <div className="w-full p-8 text-center h-[150px]">
                         Hi I'm Mitch. I'm a senior at Towson University studying
                         software engineering.
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 }
